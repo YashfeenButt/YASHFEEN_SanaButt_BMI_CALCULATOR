8de047b3a24a0a737730d48ed710c29f529b8488
YashfeenButt <163997431+YashfeenButt@users.noreply.github.com>
2024-03-19 16:09:58 +0500

    Create even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def is_even_or_odd(num):
+    if num % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+def main():
+    st.title("Even-Odd Checker")
+    num = st.number_input("Enter a number:")
+    
+    if st.button("Check"):
+        result = is_even_or_odd(num)
+        st.write(f"The number {num} is {result}.")
+
+if _name_ == "_main_":
+    main()
+  
